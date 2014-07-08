0dee21d1dc9107ae18176c9895d7004a649685e5
ldrohan <drohanluke@gmail.com>
2014-07-08 10:29:13 -0700

    added student object iteration

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -1,12 +1,30 @@
-$(window).load(function() 
-{
-   // executes when complete page is fully loaded, including all frames, objects and images
+$(window).load(function() {
 
+	var students = []
 
-   $.get( "/students.json", function( data ) {
-   		alert(data[0]["name"]);
-  		$( "#students" ).append( '<li>' + data[0]["name"] + '</li>' );  			
+  	$.get( "/students.json", function( data ) {
+   		for( var i = 0; i < data.length; i++ ){
+   			students[data[i]["id"]] = {
+   				name: data[i]["name"],
+   				bio: data[i]["bio"],
+					github: data[i]["github"],
+					linkedin: data[i]["linkedin"],
+					photo: data[i]["photo"],
+					project1: data[i]["project1"],
+					project2: data[i]["project2"],
+					project3: data[i]["project3"],
+					website: data[i]["website"],
+   			}
+  			$( "#students" ).append( '<li class="studentli" id=' + '"' + data[i]["id"] + '"'  + '>' + data[i]["name"] + '</li>' );  			
+			}
+			getinfo();
 		});
+   
+   var getinfo = function(){ $('.studentli').click(function(){
+   		var current = students[this.id]
+   		alert(current.bio);
+  	 });
+ 		}
 
 
 
