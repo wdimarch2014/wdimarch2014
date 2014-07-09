30bf45a687a8a9b90fa4f6b8d3e440a80f10ec46
ldrohan <drohanluke@gmail.com>
2014-07-08 17:59:51 -0700

    Modal working

--- app/assets/javascripts/students.js
+++ app/assets/javascripts/students.js
@@ -23,7 +23,7 @@ $(window).load(function() {
    
    var getinfo = function(){ $('.studentli').click(function(){
    		var current = students[this.id]
-   		alert(current.bio);
+   		modal.open({content: current.bio });
   	 });
  		}
 
@@ -34,13 +34,78 @@ $(window).load(function() {
       });
    });
 
- var hovereffect =function(){ $('figcaption').css('top','600px');
-  $('figure').hover(function(){
-  $(this).find('figcaption').stop().animate({'top':'160px'}, 200, function(){});
-	},function(){
-  $(this).find('figcaption').stop().animate({'top':'200px'}, 200, function(){});
-	});
-}
+ 	var hovereffect =function(){ $('figcaption').css('top','600px');
+  	$('figure').hover(function(){
+  		$(this).find('figcaption').stop().animate({'top':'160px'}, 200, function(){});
+			},function(){
+  			$(this).find('figcaption').stop().animate({'top':'200px'}, 200, function(){});
+			});
+		}	
 
 
+			var modal = (function(){
+				var 
+				method = {},
+				$overlay,
+				$modal,
+				$content,
+				$close;
+
+				// Center the modal in the viewport
+				method.center = function () {
+					var top, left;
+
+					top = Math.max($(window).height() - $modal.outerHeight(), 0) / 2;
+					left = Math.max($(window).width() - $modal.outerWidth(), 0) / 2;
+
+					$modal.css({
+						top:top + $(window).scrollTop(), 
+						left:left + $(window).scrollLeft()
+					});
+				};
+
+				// Open the modal
+				method.open = function (settings) {
+					$content.empty().append(settings.content);
+
+					$modal.css({
+						width: settings.width || 'auto', 
+						height: settings.height || 'auto'
+					});
+
+					method.center();
+					$(window).bind('resize.modal', method.center);
+					$modal.show();
+					$overlay.show();
+				};
+
+				// Close the modal
+				method.close = function () {
+					$modal.hide();
+					$overlay.hide();
+					$content.empty();
+					$(window).unbind('resize.modal');
+				};
+
+				// Generate the HTML and add it to the document
+				$overlay = $('<div id="overlay"></div>');
+				$modal = $('<div id="modal"></div>');
+				$content = $('<div id="content"></div>');
+				$close = $('<a id="close" href="#">close</a>');
+
+				$modal.hide();
+				$overlay.hide();
+				$modal.append($content, $close);
+
+				$(document).ready(function(){
+					$('body').append($overlay, $modal);						
+				});
+
+				$close.click(function(e){
+					e.preventDefault();
+					method.close();
+				});
+
+				return method;
+			}());
 });  
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
-Student.create(name:"Taylor", photo:"WDI_bw_Taylor.jpg")
-Student.create(name:"Luke", photo:"WDI_bw_Luke.jpg")
-Student.create(name:"Dave", photo:"WDI_bw_Dave.jpg")
-Student.create(name:"Josh", photo:"WDI_bw_Josh.jpg")
+Student.create(name:"Taylor", photo:"WDI_bw_Taylor.jpg", bio: "Yeoooo sonny Jim Bob my name is Taylor yhe Baylor. Bippity boppity boo wop dippity bang moredap glimrob.")
+Student.create(name:"Luke", photo:"WDI_bw_Luke.jpg", bio: "I am Luke. I am Luke. I made this crappy ass yearbook. Yeahhhhhhh. Stuff n stuff. Meep!")
+Student.create(name:"Dave", photo:"WDI_bw_Dave.jpg", bio: "Wavey Davidson. lalalalalalalalala. I made the header")
+Student.create(name:"Josh", photo:"WDI_bw_Josh.jpg", bio: "I did not help with the yearbook. I enjoy coffee. And API's.")
 
